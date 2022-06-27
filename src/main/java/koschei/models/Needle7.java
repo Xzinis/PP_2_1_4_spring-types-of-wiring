23199b8293415d56a743caa7be0b2c1a7ffb1e3e
Anastasia <doc.maos@mail.ru>
2022-06-27 15:07:42 +0300

    Full text

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -2,8 +2,15 @@ package koschei.models;
 
 public class Needle7 {
 
-    @Override
+
+    private Deth8 deth;
+
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
+
+   @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :(";
     }
 }
